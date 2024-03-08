25f5b21b94f8f20d40593cdbbac347f444289fb5
Ema <diazemanuel27@gmail.com>
2024-03-08 14:31:02 -0300

    io.Reader Interface explained

--- get-api/main.go
+++ get-api/main.go
@@ -1,63 +1,29 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
 )
 
-// {"page": "words", "input": "string1", "words": ["word1"] }
+type MySlowReader struct {
+	Contents string
+}
 
-type Words struct {
-	Page  string   `json:"page"`
-	Input string   `json:"input"`
-	Words []string `json:"words"`
+func (m MySlowReader) Read(p []byte) (n int, err error) {
+	return 0, io.EOF
 }
 
 func main() {
-	args := os.Args
-
-	if len(args) < 2 {
-		fmt.Printf("Usage ./http-get <url>\n")
-		os.Exit(1)
+	mySlowReader := MySlowReader{
+		Contents: "Hello World",
 	}
-
-	if _, err := url.ParseRequestURI(args[1]); err != nil {
-		fmt.Printf("URL is in invalid format: %s\n", err)
-		os.Exit(1)
-	}
-
-	response, err := http.Get(args[1])
+	out, err := io.ReadAll(mySlowReader)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer response.Body.Close()
-
-	body, err := io.ReadAll(response.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if response.StatusCode != 200 {
-		fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
-		os.Exit(1)
-	}
-
-	var words Words
-
-	err = json.Unmarshal(body, &words)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Json Parsed \nPage: %s\nWords: %v", words.Page, strings.Join(words.Words, ", "))
+	fmt.Printf("output: %s\n", out)
 
 }
